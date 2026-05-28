84c8038257d65c15e50d98e7e4cd0c3ae23c7937
Sumit-Shastri <shastrisumit25@gmail.com>
2026-05-28 16:05:14 +0530

    Add like_count, comment_count and link url

--- reddit-post-fetcher.py
+++ reddit-post-fetcher.py
@@ -29,20 +29,36 @@ def fetch_reddit_post(subreddit):
 
     data = r.json()
     pretty_data = json.dumps(data, indent = 4)
-    print(pretty_data)
+    #print(pretty_data)
 
     count = 1
     for i in data["data"]["children"]:
 
-        if i["data"]["selftext"] == "":
-            print("----------------------------------------------------------------\n")
-            print(f"{count}. {i["data"]["crosspost_parent_list"][0]["selftext"]}\n")
-            print("----------------------------------------------------------------\n")
+        if i["data"]["title"] == "":
+
+            post = i["data"]["crosspost_parent_list"][0]["title"]
+            likes = i["data"]["crosspost_parent_list"][0]["score"]
+            comments = i["data"]["crosspost_parent_list"][0]["num_comments"]
+            link = i["data"]["crosspost_parent_list"][0]["url"]
+
+            print("----------------------------------------------------------------")
+            print(f"{count}. {post}\n")
+            print(f"👍 Likes : {likes}  | 💬 Comments : {comments}")
+            print(f"🔗link : {link}")
+            print("----------------------------------------------------------------\n\n")
 
         else:
-            print("----------------------------------------------------------------\n")
-            print(f"{count}. {i["data"]["selftext"]}\n")
-            print("----------------------------------------------------------------\n")
+
+            post = i["data"]["title"]
+            likes = i["data"]["score"]
+            comments = i["data"]["num_comments"]
+            link = i["data"]["url"]
+
+            print("----------------------------------------------------------------")
+            print(f"{count}. {post}\n")
+            print(f"👍 Likes : {likes}  | 💬 Comments : {comments}")
+            print(f"🔗link : {link}")
+            print("----------------------------------------------------------------\n\n")
 
         count = count + 1
 
